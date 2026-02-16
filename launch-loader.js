7f84c559a6cd2456d889132a1dc20885320b8412
sankalp72-pixel <sankalp72@gmail.com>
2026-02-16 16:41:44 +0530

    launch-loader.js

--- launch-loader.js
+++ launch-loader.js
@@ -1,7 +1,8 @@
 
 (function(){
   var s = document.createElement("script");
-  s.src = "https://assets.adobedtm.com/64ac3e7fdd63/1b534d473707/launch-4a81b94b2ef0-development.min.js"; 
+  s.src = "https://assets.adobedtm.com/64ac3e7fdd63/bb60e999cd0c/launch-c4c698f85fe3-development.min.js";
   s.async = true;
   document.head.appendChild(s);
+
 })();
